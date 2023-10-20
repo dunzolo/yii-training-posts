f141a37622ee72af75d75976cd7fe6631261f357
dunzolo <rossidavide108@gmail.com>
2023-10-20 09:19:20 +0200

    add fileInput extension

--- frontend/views/post-article/create.php
+++ frontend/views/post-article/create.php
@@ -1,16 +1,20 @@
 <?php
-  use yii\helpers\Html;
-  use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\file\FileInput;
 ?>
 
 <div class="container">
 
-    <h3>Create Post</h3>
-
+  <h3>Create Post</h3>
+  
   <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
   <?php echo $form->field($model, 'title')->textInput() ?>
   <?php echo $form->field($model, 'body')->textarea(['rows' => 5]) ?>
-  <?php echo $form->field($model, 'image[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
+  <!--  --><?php //echo $form->field($model, 'image[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
+  <?php echo $form->field($model, 'image[]')->widget(FileInput::class, [
+    'options' => ['accept' => 'image/*']
+  ]); ?>
   <?php echo $form->field($model, 'status')->dropDownList([1 => 'Active', 0 => 'Inactive']) ?>
   <?php echo Html::submitButton('Post', ['class' => 'btn btn-primary']) ?>
   <?php ActiveForm::end()?>
