9f3dae2cc6ec320b07ee9688111beacdcd38515d
dunzolo <rossidavide108@gmail.com>
2023-10-20 09:40:01 +0200

    add TinyMCE extension

--- frontend/views/post-article/create.php
+++ frontend/views/post-article/create.php
@@ -2,6 +2,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\file\FileInput;
+use dosamigos\tinymce\TinyMce;
 ?>
 
 <div class="container">
@@ -10,7 +11,19 @@ use kartik\file\FileInput;
   
   <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
   <?php echo $form->field($model, 'title')->textInput() ?>
-  <?php echo $form->field($model, 'body')->textarea(['rows' => 5]) ?>
+<!--  --><?php //echo $form->field($model, 'body')->textarea(['rows' => 5]) ?>
+  <?= $form->field($model, 'body')->widget(TinyMce::class, [
+    'options' => ['rows' => 6],
+    'language' => 'en',
+    'clientOptions' => [
+      'plugins' => [
+        "advlist autolink lists link charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+      ],
+      'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+    ]
+  ]);?>
   <!--  --><?php //echo $form->field($model, 'image[]')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
   <?php echo $form->field($model, 'image[]')->widget(FileInput::class, [
     'options' => ['accept' => 'image/*']
